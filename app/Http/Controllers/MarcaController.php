6a7196e7775ac996953fb8ea72bae0d2ea2f16ff
Kevin Oliveira <kevin_costa47@hotmail.com>
2025-05-16 14:00:51 -0300

    removendo registros via delete

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -87,6 +87,7 @@ class MarcaController extends Controller
      */
     public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+        return ['msg' => 'A marca foi removida com sucesso!'];
     }
 }
