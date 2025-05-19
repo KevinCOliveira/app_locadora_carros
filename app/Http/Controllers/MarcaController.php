40443fc28e0a5e7f9f48f8fdd27d099fd343f795
Kevin Oliveira <kevin_costa47@hotmail.com>
2025-05-19 14:01:50 -0300

    implementado upload de imagem pt2

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -44,7 +44,12 @@ class MarcaController extends Controller
         $request->validate($this->marca->rules(), $this->marca->feedback());
 
         //$marca = Marca::create($request->all());
-        $marca = $this->marca->create($request->all());
+        //dd($request->nome);
+        $image = $request->imagem;
+        $image->store('imagens','public');
+        dd('Arquivo foi Enviado');
+
+        //$marca = $this->marca->create($request->all());
         return  response()->json($marca, 201);
     }
 
@@ -93,7 +98,24 @@ class MarcaController extends Controller
             return response()->json(['erro' => 'Não foi possivel atualizar. O recurso solicitado não existe'], 404);
         }
 
-        $request->validate($marca->rules(), $marca->feedback());
+        if($request->method() === 'PATCH'){
+            
+            $regrasDinamicas=array();
+
+            foreach($marca->rules() as $input => $regra){
+
+                if(array_key_exists($input, $request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }  
+            }
+            $request->validate($regrasDinamicas, $marca->feedback());  
+        }
+        else
+        {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+
+        
         $marca->update($request->all());
         return response()->json($marca, 200);
     }
