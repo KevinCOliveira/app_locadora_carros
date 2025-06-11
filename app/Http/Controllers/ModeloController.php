59760c9afc1dcccb64221c626db278f832072ff3
Kevin Oliveira <kevin_costa47@hotmail.com>
2025-06-11 16:23:14 -0300

    Implementando um Abstract Repository

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use App\Repositories\ModeloRepository;
 
 class ModeloController extends Controller
 {
@@ -18,31 +19,27 @@ class ModeloController extends Controller
      */
     public function index(Request $request)
     {
-        $modelos = array();
+        $modeloRepository = new ModeloRepository($this->modelo);
 
         if($request->has('atributos_marca')){
-            $atributos_marca = $request->atributos_marca;
-            $modelos = $this->modelo->with("marca:id,$atributos_marca");
-        }else{
-            $modelos = $this->modelo->with('marca');
+            $atributos_marca = "marca:id,$request->atributos_marca";
+            $modeloRepository->selectAtributosRegistrosRelacionados($atributos_marca); 
+        } else {
+            $modeloRepository->selectAtributosRegistrosRelacionados('marca');
         }
 
         if ($request->has('filtro')){
-            $filtros = explode(';',$request->filtro);
-            foreach($filtros as $key => $condicao){
-                $c = explode(':',$condicao);
-                $modelos = $modelos->where($c[0],$c[1],$c[2]);
+            $modeloRepository->filtro($request->filtro);
             }
-        }
 
         if($request->has('atributos')){
-            $atributos = $request->atributos;
-            $modelos = $modelos->selectRaw($atributos)->get();
-        }else{
-           $modelos = $modelos->get();
-
-        };
-        return response()->json($modelos, 200);
+            $modeloRepository->selectAtributos($request->atributos);
+        }
+        
+        //$marcas = Marca::all();
+        //$marcas = $this->marca->with('modelos')->get();
+        
+        return response()->json($modeloRepository->getResultado(), 200);
     }
 
     /**
