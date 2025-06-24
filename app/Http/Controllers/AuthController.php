3b6e8aaa38f93fd4669a7cc9f401a5cc29e016fd
Kevin Oliveira <kevin_costa47@hotmail.com>
2025-06-24 13:58:57 -0300

    implementando login e JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        
+        $credenciais = $request->all(['email','password']);
+
+        //autenticação (email e senha)
+        $token = auth('api')->attempt($credenciais);
+        
+        if($token) // usuario autenticado com sucesso
+        {
+          return response()->json(['token'=>$token],200);
+        } 
+        else //erro de usuario ou senha 
+        {
+            return response()->json(['erro'=>'Usuario ou senha incorreto'],403);
+
+            //401 = unauthorized -> não autorizado
+            //403 = forbidden -> proibido (login ínvalido)
+        }
+        //retornar um JWT
+        return 'login';
+    }
+
+    public function logout(){
+        return 'logout';
+    }
+    public function refresh(){
+        return 'refresh';
+    }
+    public function me(){
+        return 'me';
+    }
+}
