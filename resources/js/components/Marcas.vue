44c2b662c170643dd90f3596d44f4d013982c944
Kevin Oliveira <kevin_costa47@hotmail.com>
2025-07-11 14:14:28 -0300

    Criando component vue para encapsular inputs pt1

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -1,23 +1,24 @@
 <template>
+    
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Busca de Marcas</div>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Busca de Marcas</div>
 
-                    <div class="card-body">
-                         <div class="mb-3">
-                                <label for="inputId" class="form-label">ID</label>
-                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                                <div id="idHelp" class="form-text">Opcional. informe o ID de registro</div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="inputNome" class="form-label">Nome</label>
-                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
-                                <div id="nomeHelp" class="form-text">Opcional. informe o nome da marca</div>
-                            </div>
-                            <button type="submit" class="btn btn-primary brn-sm">Pesquisar</button>
-                    </div>
+                        <div class="card-body">
+                            <div class="form-row">
+                                <div class="col mb-3">
+                                    <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. informe o ID da marca">
+                                        <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                    </input-container-component>
+                                </div>  
+                        </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                            </div>       
+                        
+                        </div>
                 </div>
             </div>
         </div>
@@ -25,5 +26,6 @@
 </template>
 
 <script>
+import InputComponent from './InputContainer.vue';
  
 </script>
