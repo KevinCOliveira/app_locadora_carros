22fb9c1c0cb46e48cdbe223e107fa0cc0b6079e8
Kevin - pc-pessoal <kevin_costa47@hotmail.com>
2025-12-08 23:09:21 -0300

    instalando e configurando vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,17 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+/* importando e configurando vuex*/
+import Vuex from 'Vuex'
+
+Vue.use (Vuex)
+
+const store = new Vuex.Store({
+    state:{
+        teste:'texto de teste do Vuex'
+    }
+})
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -39,4 +50,5 @@ Vue.component('paginate-component', require('./components/Paginate.vue').default
 
 const app = new Vue({
     el: '#app',
+    store
 });
